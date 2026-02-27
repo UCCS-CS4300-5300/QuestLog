23b75d519e8476dec76f52531a8179b415c6b8b0
Darin755 <darinrawson@gmail.com>
2026-02-27 14:30:35 -0700

    redid UI

--- QuestLog/urls.py
+++ QuestLog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'QuestLog'
 
 urlpatterns = [
-    path('', views.home, name='app'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('complete_task/', views.complete_task, name='complete_task'),
 ]
--- QuestLog/views.py
+++ QuestLog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def app(request):
+def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+def complete_task(request):
+    return render(request, 'complete_task.html')
