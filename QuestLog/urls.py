3667ca895fea007c01210df156fa36e15d063960
vvitmaer <vmvitmaer@protonmail.com>
2026-03-08 13:55:32 -0600

    Added URLS.py that did not get pushed in the last commit

--- QuestLog/urls.py
+++ QuestLog/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('tasks/', views.tasks, name='tasks'),
-    path('complete_task/', views.complete_task, name='complete_task'),
+    path('complete_task', views.complete_task, name='complete_task'),
+    path('create_task', views.create_task, name='complete_task'),
+    path('parties', views.parties, name='tasks'), #join lives here
+    path('party_details', views.party_details, name='tasks'),
+    path('leaderboard', views.leaderboard, name='tasks'),
+    path('create_party', views.create_party, name='complete_task'),
+    # path('create_party/', views.complete_task, name='complete_task'),
+
 ]
+
+
