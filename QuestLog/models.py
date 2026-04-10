b743a6b02a87c99596a6b9c6a662764e04f8b271
Joe Weibel <jweibel@uccs.edu>
2026-04-09 22:25:23 -0600

    Added the model primary_key to true in the user model defintion

--- QuestLog/models.py
+++ QuestLog/models.py
@@ -18,6 +18,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        primary_key=True,
         related_name="profile",
     )
     display_name = models.CharField(max_length=150)
