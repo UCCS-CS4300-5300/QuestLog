096b2b83ada3b1ab35cbed0bb505c480218ad1d2
Darin755 <darinrawson@gmail.com>
2026-04-10 22:08:24 -0600

    remove uneeded import

--- QuestLog/views.py
+++ QuestLog/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import FileResponse, Http404, HttpResponseForbidden, response
+from django.http import FileResponse, Http404, response
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils.http import escape_leading_slashes, url_has_allowed_host_and_scheme
