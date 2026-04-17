3ea3e4894834af021ebf9ccf36f01b2ac6aba32c
Joe Weibel <jweibel@uccs.edu>
2026-04-17 11:28:54 -0600

    54-pylint-workflow

--- python_file_test.py
+++ python_file_test.py
@@ -1,7 +1,13 @@
+"""
+This file is temporary
+"""
 #!/usr/bin/env python
 
 
 def main():
+    """
+    Function to print test output
+    """
     print("This is a test file to make sure the workflow works.")
 
 
